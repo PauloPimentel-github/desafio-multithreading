2fd2bf4e1fb4c1967695d0f0b2aac4bd4d805cf2
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-10-17 00:00:49 -0300

    feat: Adicionando versão atualizada

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/PauloPimentel-github/desafio-multithreading/dto"
@@ -11,7 +13,23 @@ import (
 )
 
 func main() {
-	targetCEP := "07263725" // Você pode alterar este CEP para testar
+	// 1. Define uma flag para o CEP
+	// - nome da flag: "cep"
+	// - valor padrão: "01153000" (ou um CEP vazio se preferir obrigar o usuário a passar)
+	// - descrição: "CEP a ser consultado"
+	cepPtr := flag.String("cep", "01153000", "CEP a ser consultado. Exemplo: 01153000")
+
+	// 2. Analisa os argumentos da linha de comando
+	flag.Parse()
+
+	targetCEP := *cepPtr
+
+	// Opcional: validação básica para garantir que o CEP foi fornecido
+	if targetCEP == "" {
+		fmt.Println("Erro: O parâmetro --cep é obrigatório.")
+		flag.Usage() // Mostra a instrução de uso
+		os.Exit(1)
+	}
 
 	log.Printf("Buscando CEP %s nas duas APIs. Timeout de 1 segundo...", targetCEP)
 
